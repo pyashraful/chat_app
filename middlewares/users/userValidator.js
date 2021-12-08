3044eb1effb9bc193aac628944ecd3c1933d42e2
Py-Ashraful <ashrafulislam180@gmail.com>
2021-12-08 23:39:46 +0600

    delete file for validation error

--- middlewares/users/userValidator.js
+++ middlewares/users/userValidator.js
@@ -1,4 +1,7 @@
 const { check } = require("express-validator");
+const createError = require("http-errors");
+const path = require("path");
+const { unlink } = require("fs");
 
 const addUserValidator = [
   check("name")
@@ -18,6 +21,45 @@ const addUserValidator = [
         if (userEmail) {
           throw createError("Email already exists");
         }
-      } catch (error) {}
+      } catch (err) {
+        throw createError(err.message);
+      }
     }),
+
+  check("mobile")
+    .isMobilePhone("bn-BD", { strictMode: true })
+    .withMessage("Moblie number must be a valid bangladeshi mobile number")
+    .custom(async (value) => {
+      try {
+        const userMobile = await User.findOne({ mobile: value });
+        if (userMobile) {
+          throw createError("Mobile already is use!");
+        }
+      } catch (err) {
+        throw createError(err.message);
+      }
+    }),
+
+  check("password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"
+    ),
 ];
+
+function addUserValidationHendler(req, res, next) {
+  const errors = validationResult(req);
+  const mappedErrors = errors.mapped();
+  if (Object.keys(mappedErrors).length === 0) {
+    next();
+  } else {
+    if (req.files.length > 0) {
+      const fileName = req.files[0];
+      unlink(path.join(__dirname, `/../public/uploads/avatars/${fileName}`));
+    }
+  }
+}
+
+module.exports = {
+  addUserValidator,
+};
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { getUsers } = require("../controller/userscontroller");
 const decoratedHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const { addUserValidator } = require("../middlewares/users/userValidator");
 
 const router = express.Router();
 
 router.get("/", decoratedHtmlResponse("users"), getUsers);
+router.post("/", avatarUpload, addUserValidator);
 
 module.exports = router;
